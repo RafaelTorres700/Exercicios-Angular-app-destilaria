0b768b0f677c1ecb8ac025fffae34db860d85fb1
RafaelTorres700 <torres91032@gmail.com>
2025-07-15 15:23:35 -0300

    1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,31 +4,38 @@ import { ServicosComponent } from './servicos/servicos.component';
 import { ContatoComponent } from './contato/contato.component';
 import { SobreComponent } from '../sobre/sobre.component';
 import { PainelComponent } from '../painel/painel.component';
+import { authGuard } from './guards/auth.guard';
 
 
 export const routes: Routes = [
 
-
-  {
-    path: '',
-    component: HomeComponent
-  },
 {
-    path: 'painel',
-    component: PainelComponent
+    path: '',
+    component: PainelComponent,
+    
+
 },
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [authGuard]
+  },
+
 
   {
     path: 'servicos',
-    component: ServicosComponent
+    component: ServicosComponent,
+    canActivate: [authGuard]
   },
   {
     path: 'contato',
-    component: ContatoComponent
+    component: ContatoComponent,
+    canActivate: [authGuard]
   },
 
   {
     path: 'sobre',
-    component: SobreComponent
+    component: SobreComponent,
+    canActivate: [authGuard]
   }
 ];
--- src/painel/painel.component.ts
+++ src/painel/painel.component.ts
@@ -1,27 +1,38 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../app/services/auth.service';
-import { Router, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-painel',
-  imports: [FormsModule, RouterModule, CommonModule],
+  imports: [FormsModule, ReactiveFormsModule,RouterModule, CommonModule],
   templateUrl: './painel.component.html',
   styleUrl: './painel.component.css',
   providers: [AuthService]
 })
 export class PainelComponent {
-usuario = '';
- senha = '';
- erro = false;
- constructor(private auth: AuthService, private router: Router) {}
- entrar() {
- if (this.auth.login(this.usuario, this.senha)) {
- this.router.navigate(['']);
- } else {
- this.erro = true;
- }
- }
+  cadastroForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.cadastroForm = this.fb.group({
+      nome: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]],
+      dataNascimento: ['', Validators.required],
+      telefone: ['', Validators.required]
+    });
+  }
+
+  enviarCadastro() {
+    if (this.cadastroForm.valid) {
+      console.log(this.cadastroForm.value);
+      alert('Formulário enviado com sucesso!');
+    } else {
+      alert('Formulário inválido');
+    }
+  }
+
 }
